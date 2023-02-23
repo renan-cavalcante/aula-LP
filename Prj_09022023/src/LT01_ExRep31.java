58053a0744d155d3a0833566078e66d569ba3d81
renan-cavalcante <renancggama@gmail.com>
2023-02-23 20:27:36 -0300

    Exercicio 31

---
+++ Prj_09022023/src/LT01_ExRep31.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+/*
+    programador: Renan Gama
+    Data criação: 23/02/2023
+    versão 01
+ */
+public class LT01_ExRep31 {
+    public static void main(String arqs[]){
+        int indice;
+        
+        for (indice = 10; indice <= 150; indice++){
+            System.out.println("O quadrado de " +indice+" é "+indice*indice);
+        }
+    }
+}
