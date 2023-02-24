01cc1810fa421a0ce8e0348a3b41b2d34a2e2b5f
renan-cavalcante <renancggama@gmail.com>
2023-02-23 21:31:38 -0300

    Exercicio 37

--- Prj_09022023/src/LT01_ExRep37.java
+++ Prj_09022023/src/LT01_ExRep37.java
@@ -1,12 +1,27 @@
+import javax.swing.JOptionPane;
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author FATEC ZONA LESTE
+    programador: Renan Gama
+    Data criação: 23/02/2023
+    versão 01
  */
 public class LT01_ExRep37 {
-    
+    public static void main(String arqs[]){
+        int numero, indice, F1,F2, fibo;
+        
+        numero = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero"));
+        
+        F1 = 0;
+        F2 = 1;
+        indice = 1;
+        
+        System.out.print(F2+", ");
+        
+        while(indice < numero){
+            fibo = F1 + F2;
+            F1 = F2;
+            F2 = fibo;
+            indice++;
+            System.out.print(fibo+", ");
+        }
+    }
 }
