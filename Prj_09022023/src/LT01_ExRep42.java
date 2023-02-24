38d1c64df3e52b2e96a91c2cf7661fb3d6c21b3e
renan-cavalcante <renancggama@gmail.com>
2023-02-23 22:31:21 -0300

    Exercicio 42

---
+++ Prj_09022023/src/LT01_ExRep42.java
@@ -0,0 +1,22 @@
+/*
+    programador: Renan Gama
+    Data criação: 23/02/2023
+    versão 01
+ */
+public class LT01_ExRep42 {
+    public static void main(String arqs[]){
+        double indice, serie, contador;
+        
+        serie = 1;
+        contador =1;
+        
+        for (indice = 2; indice <= 50; indice++){
+            contador+=2;
+            serie = serie +( indice/contador);
+        }
+        
+        System.out.println("O resultado da série é "+serie);
+    }
+}
+
+
