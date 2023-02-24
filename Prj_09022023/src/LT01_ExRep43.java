a07b7b7b36acbdfc34ebf98f52e254c1c725cb26
renan-cavalcante <renancggama@gmail.com>
2023-02-23 22:35:01 -0300

    Exercicio 43 não finalizado

--- Prj_09022023/src/LT01_ExRep43.java
+++ Prj_09022023/src/LT01_ExRep43.java
@@ -1,12 +1,11 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author FATEC ZONA LESTE
+    programador: Renan Gama
+    Data criação: 23/02/2023
+    versão 01
  */
 public class LT01_ExRep43 {
-    
+    public static void main(String arqs[]){
+        double AlturaAna, AlturaMaria, Anos;
+        boolean passou;
+    }
 }
