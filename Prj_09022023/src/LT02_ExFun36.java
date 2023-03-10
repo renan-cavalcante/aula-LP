4e10ce22ee3b67e4f29cad4e93d5ddf4285a4d2d
renan-cavalcante <renancggama@gmail.com>
2023-03-09 21:01:12 -0300

    Exercicio 36 lote 2

---
+++ Prj_09022023/src/LT02_ExFun36.java
@@ -0,0 +1,33 @@
+import javax.swing.JOptionPane;
+
+/*
+    @author: Renan Gama
+    Data criação: 09/03/2023
+    versão 01
+ */
+
+public class LT02_ExFun36 {
+    public static void main(String arqs[]){
+        int numero ;
+        
+        numero = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero"));
+        
+        
+        System.out.println("O resultado da série é "+calcularSerie((double)numero));
+    }
+    
+    static double calcularSerie(double n){
+        double indice, serie,fatorial;
+        serie = 1;
+        indice = 1;
+        fatorial = 1;
+        
+        while(indice <= n){
+            fatorial = fatorial*indice;
+            serie = serie + (1/fatorial);
+            indice++;
+        }
+        return serie;  
+    }
+}
+
